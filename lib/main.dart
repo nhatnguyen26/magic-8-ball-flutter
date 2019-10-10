4195c669d50e2f7f548769d3b0234797454c0ede
nhatnguyen <nhatnguyen@ebay.com>
2019-10-10 16:37:57 -0700

    implement magic 8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueAccent[100],
+          appBar: AppBar(
+            title: Text('Decision Maker'),
+            backgroundColor: Colors.blueAccent[400],
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  BallPageState createState() {
+    return BallPageState();
+  }
+}
+
+class BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+  Random random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: imageClick,
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+
+  void imageClick() {
+    setState(() {
+      int temp = 1;
+      do {
+        temp = random.nextInt(5) + 1;
+      } while (temp == ballNumber);
+      ballNumber = temp;
+    });
+  }
+}
